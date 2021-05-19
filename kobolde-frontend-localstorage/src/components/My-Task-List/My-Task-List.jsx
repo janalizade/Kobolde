2ae86f837877ca7879b11277304d25bb914959d6
janalizade <janalizade@gmail.com>
2021-05-19 14:35:03 +0200

    localstorage added

--- kobolde-frontend-localstorage/src/components/My-Task-List/My-Task-List.jsx
+++ kobolde-frontend-localstorage/src/components/My-Task-List/My-Task-List.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
 import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles, createStyles} from '@material-ui/core/styles';
 import "./my-task-list.css";
-import logo from '../../assets/images/logo.png';
+import { Offline, Online } from "react-detect-offline"
+import AppBar from 'material-ui/AppBar';
+import axios from 'axios';
+import { base64StringToBlob } from 'blob-util';
 import {
-  Paper,
   Button,
   TextField,
+  Paper,
   Box,
   Typography,
-  Grid,
-  FormControlLabel,
+  Grid
+
 } from "@material-ui/core";
+import logo from '../My-Task-List/logo.png';
+import { withStyles } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import formData from 'form-data';
 const styles = theme => ({
  
   paper: {
+    
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
@@ -29,10 +40,13 @@ const styles = theme => ({
     marginTop: theme.spacing(10),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    width: '100%',
+    marginTop: theme.spacing(2),
+    
   },
   root: {
     width: '100%',
+    maxWidth: 1800,
     marginTop: theme.spacing.unit * 3,
     overflowX: 'auto',
   },
@@ -43,29 +57,44 @@ const styles = theme => ({
     marginTop:350,
   },
   button: {
-    margin: theme.spacing(1),
+   backgroundColor:'orange',
+   width: '100%',
+   marginTop: theme.spacing(2),
+  
   },
 });
 
-
 class MyTaskList extends Component {
+ 
   constructor(props) {
     super(props);
     this.state = { file: null };
     this.state = {
       task: "",
-      productImage: null ,
-      serialNo:"",
-      tasklist: []
+      taskCategory:"",
+      taskSerialNo:"",
+      taskImage: { file: null } ,
+       tasklist: [],
+       connsctionStatus:"",
+       selection : 1
     };
-    this.onchangeImage = this.onchangeImage.bind(this);
+    window.localStorage.clear();
   }
  
   // on load get the task list
   componentDidMount = () => {
+    var status=navigator.onLine;
+    if(status) {
+     this.setState({connectionStatus:"online"});
+  
+    }else{
+      this.setState({connectionStatus:"offline"});
+      }
     this.getTasks();
   };
 
+
+  
   onChange = event => {
   
     this.setState({
@@ -73,19 +102,112 @@ class MyTaskList extends Component {
     });
   };
 
+
+  getBase64 = file => {
+    return new Promise(resolve => {
+      let fileInfo;
+      let baseURL = "";
+      // Make new FileReader
+      let reader = new FileReader();
+
+      // Convert the file to base64 text
+      reader.readAsDataURL(file);
+
+      // on reader load somthing...
+      reader.onload = () => {
+        // Make a fileInfo Object
+        console.log("Called", reader);
+        baseURL = reader.result;
+        console.log(baseURL);
+        resolve(baseURL);
+      };
+      console.log(fileInfo);
+    });
+  };
+ 
+  handleFileInputChange = e => {
+   
+    console.log(e.target.files[0]);
+    let { file } = this.state;
+
+    file = e.target.files[0];
+
+    this.getBase64(file)
+      .then(result => {
+        file["base64"] = result;
+        console.log("File Is", result);
+        this.setState({
+          base64URL: result,
+          file
+        });
+        this.setState({
+          taskImage: result
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    this.setState({
+      file: e.target.files[0]
+    });
+  
+  };
+
+
+
+
   onchangeImage=event=>{
     this.setState({
       file: URL.createObjectURL(event.target.files[0])
     });
    
     this.setState({
-      productImage: URL.createObjectURL(event.target.files[0])
+      taskImage: event.target.files[0]
     });
         
   }
+  
+onSync=()=>{
+  let tasklist = JSON.parse(localStorage.getItem("tasklist"));
+     tasklist.map((item, index) => {
+      console.log("item task",item);
+     console.log("item task",item.task);
+     console.log("item serialNo",item.taskSerialNo);
+     console.log("item image",item.taskImage.constructor);
+      var formdata =new formData();
+      formdata.append('title',item.task);
+      formdata.append('serialNo',item.taskSerialNo);
+      const contentType = 'image/png';
+      const data = item.taskImage.split('base64,')[1];
+      const blob = base64StringToBlob(data, contentType);
+      console.log(blob);
+      formdata.append('image',blob);
+         var config = {
+          method: 'post',
+          url: 'http://localhost:8000/api/v1/admin/productx',
+          headers: { 
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          data : formdata
+        };
+        
+        axios(config)
+         .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    
+    });
+  }
+  
+
+
   // add task to the list
   onSubmit = () => {
-    //debugger
+
     // check is task is empty string
     if (this.state.task) {
       // get the task list from the local storage
@@ -99,11 +221,15 @@ class MyTaskList extends Component {
 
       // create task object
       // default status is false
-
+  
+      
+      
       let task = {
-        task: `📄 ${this.state.task}`,
-        productImage:`${this.state.productImage}`,
-        serialNo:`${this.state.serialNo}`,
+        
+        task: ` ${this.state.task}`,
+        taskCategory: `${this.state.taskCategory}`,
+        taskSerialNo: ` ${this.state.taskSerialNo}`,
+        taskImage:` ${this.state.taskImage}`,
          status: false
       };
     
@@ -112,12 +238,15 @@ class MyTaskList extends Component {
       tasklist.push(task);
 
       // save the task list in the local storage
+    
       localStorage.setItem("tasklist", JSON.stringify(tasklist));
 
       // clear the form
       this.setState({ task: "" });
-      this.setState({ serialNo: "" });
-      this.setState({ productImage: "" });
+      this.setState({taskCategory:""});
+      this.setState({taskSerialNo:""});
+      this.setState({taskImage:""});
+
       // refresh the tasks
       this.getTasks();
     }
@@ -127,7 +256,7 @@ class MyTaskList extends Component {
   getTasks = () => {
     // get the task list from the local storage
     let tasklist = JSON.parse(localStorage.getItem("tasklist"));
-
+    console.log(tasklist);
     // check if task list is empty
     if (tasklist) {
       // sort all the tasks on the basis of status
@@ -160,12 +289,14 @@ class MyTaskList extends Component {
             taskComplete["textDecoration"] = "line-through";
           }
           return (
+        
             <Card key={index} color={color} fluid style={cardBackground}>
               <Card.Content>
                 <Card.Header textAlign="left" style={taskComplete}>
-                  <div style={{ wordWrap: "break-word" }}>{item.task}</div>
-                  <div style={{ wordWrap: "break-word" }}>{item.serialNo}</div>
-                  <img style={{ width: "10%" }} src={item.productImage}/>
+                  <div style={{ wordWrap: "break-word" }}>motor : {item.task}</div>
+                  <div style={{ wordWrap: "break-word" }}>category: {item.taskCategory}</div>
+                  <div style={{ wordWrap: "break-word" }}>serialNo:{item.taskSerialNo}</div>
+                  <img style={{ width: "40%" }} src={item.taskImage}/>
                 </Card.Header>
 
                 <Card.Meta textAlign="right">
@@ -193,6 +324,7 @@ class MyTaskList extends Component {
                 </Card.Meta>
               </Card.Content>
             </Card>
+          
           );
         })
       });
@@ -235,17 +367,18 @@ class MyTaskList extends Component {
     this.getTasks();
   };
 
-  render() { const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
     return (
-      <div>
+      
       <div className={classes.form}>
-      <Box mt={5} px={3}>
-      <Grid container spacing={3}
+      <Box mt={7} px={8}>
+      <Grid container spacing={6}
         direction="row"
         justify="center"
         alignItems="center">
-        <Grid item xs={12} sm={8} md={6} lg={5}>
-          <Paper className={classes.paper}>
+        <Grid item xs={12} sm={4} md={6} lg={5}>
+         <Paper className={classes.paper}>
           <Typography variant="h3" gutterBottom>
             <img src={logo}/>
           </Typography>
@@ -274,45 +407,38 @@ class MyTaskList extends Component {
               type="text"
               name="taskSerialNo"
               onChange={this.onChange}
-              value={this.state.serialNo}
+              value={this.state.taskSerialNo}
               fluid
               placeholder="serialNo..."
             />
-              <input type="file" name="file"  onChange={this.onchangeImage} />
-              <Button
-                 type="submit"
-                 className={classes.button}
-                 fullWidth
-                 variant="contained"
-                 color="primary"
-                 onClick={this. updateQuery}
-                  >
-                Lägg till ny bild
-                </Button>
-              <Button
-                 type="submit"
-                 className={classes.submit}
-                 fullWidth
-                 variant="contained"
-                 color="primary"
-                  >
-                Lägg till list
-                </Button>
-          </Form>
-       
-        <div>
+              <input type="file" name="file"  onChange={this.handleFileInputChange} />
+              
+            
+          
+        <div className={classes.root}>
+        <MuiThemeProvider >
+         <DropDownMenu 
+          value={this.state.selection} 
+          
+         >
+          <MenuItem value={1} primaryText="Lägg till list"   onClick={this.onSubmit}/>
+          <MenuItem value={2} primaryText="Sync" onClick={this.onSync} />
+          
+        </DropDownMenu>
+        <br/><br/><br/>
+    
+        </MuiThemeProvider>
+        </div>        
           <Card.Group className={classes.paper}>{this.state.tasklist}</Card.Group>
-        </div>
-   
+      
+        </Form>
       </Paper>
       </Grid>
       </Grid>
       </Box>
       </div>
-       </div>
     );
   }
 }
-
 
 export default withStyles(styles)(MyTaskList);
